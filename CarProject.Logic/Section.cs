3ad36cf38325ff1221123cacd0587d419d8f7d5e
School <School@PA-650G8-NB-SCH>
2024-11-06 14:48:04 +0100

    Add project files.

---
+++ CarProject.Logic/Section.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarProject.Logic
+{
+    public class Section
+    {
+        #region properties
+        public int MaxSpeed { get; set; }
+        public int Length { get; set; }
+        public Section? NextSection { get; set; }
+        public Section? PreviousSection { get; set; }
+
+        #endregion properties
+
+        public Section(int speed, int length)
+        {
+            MaxSpeed = speed;
+            Length = length;
+        }
+
+        public void AddAfterMe(Section section)
+        {
+            Section? tmp = NextSection;
+
+            NextSection = section;
+
+            section.NextSection = tmp;
+        }
+
+        public void AddBeforeMe(Section section)
+        {
+            PreviousSection = section;
+        }
+    }
+}
---
+++ CarProject.UnitTests/SectionTest.cs
@@ -0,0 +1,61 @@
+﻿using CarProject.Logic;
+namespace CarProject.UnitTests
+{
+    [TestClass]
+    public class SectionTest
+    {
+        [TestMethod]
+        public void ItShouldHaveALengthAndAMaxSpeed_GivenObjectCreated()
+        {
+            var someSpeed = 60;
+            var someLength = 400;
+            Section section = new Section(someSpeed, someLength);
+
+            Assert.AreEqual(someSpeed, section.MaxSpeed);
+            Assert.AreEqual(someLength, section.Length);
+        }
+        [TestMethod]
+        public void ItShouldConnectASectionAfterTheCurrentSection_GivenAddAfterMeIsCalled()
+        {
+            Section section = new Section(60, 400);
+            Section nextSection = new Section(60, 400);
+            section.AddAfterMe(nextSection);
+
+            Assert.AreEqual(nextSection, section.NextSection);
+        }
+        [TestMethod]
+        public void ItShouldConnectASectionBeforeTheCurrentSection_GivenAddBeforeMeIsCalled()
+        {
+            Section section = new Section(60, 400);
+            Section previousSection = new Section(60, 400);
+            section.AddBeforeMe(previousSection);
+
+            Assert.AreEqual(previousSection, section.PreviousSection);
+        }
+        [TestMethod]
+        public void ItShouldInsertASectionBetweenTwoSections_GivenTwoConnectedSectionsAndAddAfterMeIsCalled()
+        {
+            Section sectionOne = new Section(60, 400);
+            Section sectionTwo = new Section(60, 500);
+            Section insertSection = new Section(50, 300);
+
+            sectionOne.AddAfterMe(sectionTwo);
+            sectionOne.AddAfterMe(insertSection);
+
+            Assert.AreEqual(sectionTwo, sectionOne.NextSection.NextSection);
+        }
+
+        [TestMethod]
+        public void ItShouldInsertASectionBetweenTwoSections_GivenTwoConnectedSectionsAndAddbeforeMeIsCalled()
+        {
+            Section sectionOne = new Section(60, 400);
+            Section sectionTwo = new Section(60, 500);
+            Section insertSection = new Section(50, 300);
+
+            sectionOne.AddAfterMe(sectionTwo);
+            sectionTwo.AddBeforeMe(insertSection);
+
+            Assert.AreEqual(sectionTwo, sectionOne.NextSection.NextSection);
+        }
+    }
+}
